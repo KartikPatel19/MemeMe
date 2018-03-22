4965ea253f378ce1a853a64a2295ee8e81b54007
kartikpatel <patelkartik1910@gmail.com>
2018-03-23 00:00:41 +0530

    Initial Commit

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,158 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate, UITextFieldDelegate{
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var topToolbar: UIToolbar!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    
+    
+    struct Meme {
+        let topText: String
+        let bottomText: String
+        let originalImage: UIImage
+        let memedImage: UIImage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        let memeTextAttributes:[String: Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.white,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.black,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedStringKey.strokeWidth.rawValue: UIColor.red
+        ]
+        
+        topText.text = "TOP"
+        bottomText.text = "BOTTOM"
+        
+        topText.delegate = self
+        bottomText.delegate = self
+        
+        topText.typingAttributes = memeTextAttributes
+        bottomText.typingAttributes = memeTextAttributes
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+        if textField == topText{
+            view.frame.origin.y = 0
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeybordNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeToKeybordNotification()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @objc func keybordWillShow(_ notification: Notification){
+        view.frame.origin.y = -getKeybordHeight(notification)
+    }
+    
+    @objc func keybordWillHide(_ notification: Notification){
+        view.frame.origin.y = 0
+    }
+    
+    func getKeybordHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyBordSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyBordSize.cgRectValue.height
+        
+    }
+    
+    @IBAction func cameraAction(_ sender: Any) {
+    }
+    
+    func subscribeToKeybordNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillShow(_:)),
+                                               name: .UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillHide(_:)),
+                                               name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeybordNotification(){
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @IBAction func shareImageButton(_ sender: UIBarButtonItem) {
+        
+        // image to share
+        let image = generateMemedImage()
+        
+        // set up activity view controller
+        let imageToShare = [ image ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveImage(_ sender: Any) {
+        let meme = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imagePickerView.image!, memedImage: generateMemedImage())
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        topToolbar.isHidden = true
+        bottomToolbar.isHidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        topToolbar.isHidden = false
+        bottomToolbar.isHidden = false
+        
+        
+        return memedImage
+    }
+    
 }
 
